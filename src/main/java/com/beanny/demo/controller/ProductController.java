905bae59630278847055cf01c316f4038448962a
Yim Sotharoth <yimsotharoth999@gmail.com>
2025-07-16 21:51:31 +0700

    initial project

---
+++ src/main/java/com/beanny/demo/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.beanny.demo.controller;
+
+import com.beanny.demo.model.BaseResponseModel;
+import com.beanny.demo.model.BaseResponseWithDataModel;
+import com.beanny.demo.model.ProductModel;
+import com.beanny.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+    
+    @GetMapping
+    public ResponseEntity<BaseResponseWithDataModel> listProducts() {
+        return productService.listProducts();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<BaseResponseWithDataModel> getProduct(@PathVariable("id") Long productId) {
+        return productService.getProduct(productId);
+    }
+    
+    @GetMapping("/search")
+    public ResponseEntity<BaseResponseWithDataModel> searchProductsByFilters(
+            @RequestParam(value = "name", required = false) String name
+    ) {
+        return productService.searchProducts(name);
+    }
+    
+    @PostMapping
+    public ResponseEntity<BaseResponseModel> createProduct(@RequestBody ProductModel payload) {
+        return productService.createProduct(payload);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<BaseResponseModel> updateProduct(@PathVariable("id") Long productId,@RequestBody ProductModel payload) {
+        return productService.updateProduct(productId,payload);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BaseResponseModel> deleteProduct(@PathVariable("id") Long productId) {
+        return productService.deleteProduct(productId);
+    }
+}
---
+++ src/main/java/com/beanny/demo/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.beanny.demo.repository;
+
+import com.beanny.demo.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    @Query("SELECT p FROM Product p WHERE :name IS NULL OR LOWER(p.productName) LIKE %:name%")
+    List<Product> findProductsWithFilters(@Param("name") String name);
+}
