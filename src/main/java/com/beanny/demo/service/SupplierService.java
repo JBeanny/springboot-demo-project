032d2e435837fddb530febe9b2d8471c88b6325f
Yim Sotharoth <yimsotharoth999@gmail.com>
2025-07-30 21:52:50 +0700

    feat: add supplier implementation

--- src/main/java/com/beanny/demo/service/SupplierService.java
+++ src/main/java/com/beanny/demo/service/SupplierService.java
@@ -1,7 +1,84 @@
 package com.beanny.demo.service;
 
+import com.beanny.demo.dto.supplier.SupplierDto;
+import com.beanny.demo.dto.supplier.UpdateSupplierDto;
+import com.beanny.demo.entity.Supplier;
+import com.beanny.demo.mapper.SupplierMapper;
+import com.beanny.demo.model.BaseResponseModel;
+import com.beanny.demo.model.BaseResponseWithDataModel;
+import com.beanny.demo.repository.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SupplierService {
+    @Autowired
+    private SupplierRepository supplierRepository;
+    
+    @Autowired
+    private SupplierMapper mapper;
+    
+    public ResponseEntity<BaseResponseWithDataModel> listSuppliers() {
+        List<Supplier> suppliers = supplierRepository.findAll();
+        
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(
+                        new BaseResponseWithDataModel(
+                                "success",
+                                "successfully retrieved suppliers",
+                                mapper.toDtoList(suppliers)
+                        )
+                );
+    }
+    
+    public ResponseEntity<BaseResponseModel> createSupplier(SupplierDto dto) {
+        // if duplicate supplier name , then reject
+        if(supplierRepository.existsByName(dto.getName())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(new BaseResponseModel("fail","supplier already existed with name: " + dto.getName()));
+        }
+        
+        Supplier supplier = mapper.toEntity(dto);
+        
+        supplierRepository.save(supplier);
+        
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new BaseResponseModel("success","successfully created supplier"));
+    }
+    
+    public ResponseEntity<BaseResponseModel> updateSupplier(Long supplierId, UpdateSupplierDto dto) {
+        Optional<Supplier> existingSupplier = supplierRepository.findById(supplierId);
+        
+        // if supplier not found, return 404
+        if(existingSupplier.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new BaseResponseModel("fail","supplier not found with id: " + supplierId));
+        }
+        
+        Supplier supplier = existingSupplier.get();
+        mapper.updateEntityFromDto(supplier,dto);
+        
+        supplierRepository.save(supplier);
+        
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new BaseResponseModel("success","successfully updated supplier"));
+    }
+    
+    public ResponseEntity<BaseResponseModel> deleteSupplier(Long supplierId) {
+        if(!supplierRepository.existsById(supplierId)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new BaseResponseModel("fail","supplier not found with id: " + supplierId));
+        }
+        
+        supplierRepository.deleteById(supplierId);
+        
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new BaseResponseModel("success","successfully deleted supplier"));
+    }
+    
 }
