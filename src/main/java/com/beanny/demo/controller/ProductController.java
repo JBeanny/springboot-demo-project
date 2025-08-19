663608bb0e727e05cd961820c7f9e33ca593d633
Yim Sotharoth <yimsotharoth999@gmail.com>
2025-08-19 21:53:18 +0700

    refactor(product): change to use Response model for API response

--- src/main/java/com/beanny/demo/controller/ProductController.java
+++ src/main/java/com/beanny/demo/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.beanny.demo.controller;
 
+import com.beanny.demo.dto.base.Response;
 import com.beanny.demo.dto.product.ProductDto;
 import com.beanny.demo.dto.product.ProductResponseDto;
 import com.beanny.demo.model.BaseResponseModel;
@@ -20,23 +21,23 @@ public class ProductController {
     private ProductService productService;
     
     @GetMapping
-    public ResponseEntity<BaseResponseWithDataModel> listProducts() {
+    public ResponseEntity<Response> listProducts() {
         List<ProductResponseDto> products = productService.listProducts();
         
         return ResponseEntity.status(HttpStatus.OK)
-                .body(new BaseResponseWithDataModel("success","successfully retrieved products",products));
+                .body(Response.success("200","success","successfully retrieved products",products));
     }
     
     @GetMapping("/{id}")
-    public ResponseEntity<BaseResponseWithDataModel> getProduct(@PathVariable("id") Long productId) {
+    public ResponseEntity<Response> getProduct(@PathVariable("id") Long productId) {
         ProductResponseDto product = productService.getProduct(productId);
         
         return ResponseEntity.status(HttpStatus.OK)
-                .body(new BaseResponseWithDataModel("success","product found",product));
+                .body(Response.success("200","success","product found",product));
     }
     
     @GetMapping("/search")
-    public ResponseEntity<BaseResponseWithDataModel> searchProductsByFilters(
+    public ResponseEntity<Response> searchProductsByFilters(
             @RequestParam(value = "name", required = false) String name,
             @RequestParam(value = "minPrice", required = false) Double minPrice,
             @RequestParam(value = "maxPrice", required = false) Double maxPrice
@@ -44,30 +45,30 @@ public class ProductController {
         List<ProductResponseDto> products = productService.searchProducts(name,minPrice,maxPrice);
         
         return ResponseEntity.status(HttpStatus.OK)
-                .body(new BaseResponseWithDataModel("success","successfully retrieved products with filters",products));
+                .body(Response.success("200","success","successfully retrieved products with filters",products));
     }
     
     @PostMapping
-    public ResponseEntity<BaseResponseModel> createProduct(@Valid @RequestBody ProductDto payload) {
+    public ResponseEntity<Response> createProduct(@Valid @RequestBody ProductDto payload) {
         productService.createProduct(payload);
         
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new BaseResponseModel("success","successfully created product"));
+                .body(Response.success("201","success","successfully created product"));
     }
     
     @PutMapping("/{id}")
-    public ResponseEntity<BaseResponseModel> updateProduct(@PathVariable("id") Long productId,@RequestBody ProductDto payload) {
+    public ResponseEntity<Response> updateProduct(@PathVariable("id") Long productId,@RequestBody ProductDto payload) {
         productService.updateProduct(productId,payload);
         
         return ResponseEntity.status(HttpStatus.OK)
-                .body(new BaseResponseModel("success","successfully updated product"));
+                .body(Response.success("200","success","successfully updated product"));
     }
     
     @DeleteMapping("/{id}")
-    public ResponseEntity<BaseResponseModel> deleteProduct(@PathVariable("id") Long productId) {
+    public ResponseEntity<Response> deleteProduct(@PathVariable("id") Long productId) {
         productService.deleteProduct(productId);
         
         return ResponseEntity.status(HttpStatus.OK)
-                .body(new BaseResponseModel("success","successfully deleted product id: " + productId));
+                .body(Response.success("200","success","successfully deleted product id: " + productId));
     }
 }
