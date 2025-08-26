cf259dbbba3c488e08defb4d9ac7cd0531c20ee2
Yim Sotharoth <yimsotharoth999@gmail.com>
2025-08-26 22:03:03 +0700

    feat: create LogFormatter for consistent log

--- src/main/java/com/beanny/demo/common/logging/LoggingAspect.java
+++ src/main/java/com/beanny/demo/common/logging/LoggingAspect.java
@@ -3,14 +3,20 @@ package com.beanny.demo.common.logging;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.hibernate.id.uuid.UuidGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Aspect
 @Component
 public class LoggingAspect {
     String LOG_FORMAT = "%s | className=%s, method=%s";
+    @Autowired
+    private LogFormatter formatter;
     
     @Around("execution(* com.beanny.demo.service..*(..))")
     public Object logServiceMethods(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -19,12 +25,25 @@ public class LoggingAspect {
         String methodName = joinPoint.getSignature().getName();
         // get class name , e.g. StockService
         String className = joinPoint.getTarget().getClass().getSimpleName();
+        long startTime = System.currentTimeMillis();
+        String requestId = UUID.randomUUID().toString();
         
-        log.info(LOG_FORMAT.formatted("Request",className,methodName));
+        log.info(formatter.logRequest(requestId,className,methodName,startTime));
         
         try {
-            return joinPoint.proceed();
+            // execute the original method logic
+            Object result = joinPoint.proceed();
+            
+            long endTime = System.currentTimeMillis();
+            // logging
+            log.info(formatter.logResponse(requestId,className,methodName,startTime,endTime));
+            
+            return result;
         } catch (Exception e) {
+            long endTime = System.currentTimeMillis();
+            
+            log.error(formatter.logError(requestId,className,methodName,startTime,endTime));
+            
             throw e;
         }
     }
