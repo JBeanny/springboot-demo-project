dd1e438e354673f1ee494cab941cd9f99eb5e104
Yim Sotharoth <yimsotharoth999@gmail.com>
2025-08-27 21:30:00 +0700

    feat: add logs for controller and repository

--- src/main/java/com/beanny/demo/common/logging/LoggingAspect.java
+++ src/main/java/com/beanny/demo/common/logging/LoggingAspect.java
@@ -24,11 +24,11 @@ public class LoggingAspect {
         // get method name , e.g. listStocks()
         String methodName = joinPoint.getSignature().getName();
         // get class name , e.g. StockService
-        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String target = joinPoint.getTarget().getClass().getSimpleName();
         long startTime = System.currentTimeMillis();
         String requestId = UUID.randomUUID().toString();
         
-        log.info(formatter.logRequest(requestId,className,methodName,startTime));
+        log.info(formatter.logRequest(requestId,target,methodName,startTime));
         
         try {
             // execute the original method logic
@@ -36,13 +36,67 @@ public class LoggingAspect {
             
             long endTime = System.currentTimeMillis();
             // logging
-            log.info(formatter.logResponse(requestId,className,methodName,startTime,endTime));
+            log.info(formatter.logResponse(requestId,target,methodName,startTime,endTime));
             
             return result;
         } catch (Exception e) {
             long endTime = System.currentTimeMillis();
             
-            log.error(formatter.logError(requestId,className,methodName,startTime,endTime));
+            log.error(formatter.logError(requestId,target,methodName,startTime,endTime));
+            
+            throw e;
+        }
+    }
+    
+    @Around("execution(* com.beanny.demo.repository..*(..))")
+    public Object logRepositoryMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+        Logger log = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        String methodName = joinPoint.getSignature().getName();
+        String target = joinPoint.getTarget().getClass().getSimpleName();
+        long startTime = System.currentTimeMillis();
+        String requestId = UUID.randomUUID().toString();
+        
+        log.info(formatter.logRequest(requestId,target,methodName,startTime));
+        
+        try {
+            Object result = joinPoint.proceed();
+            long endTime = System.currentTimeMillis();
+            
+            log.info(formatter.logResponse(requestId,target,methodName,startTime,endTime));
+            
+            return result;
+        } catch (Exception e) {
+            long endTime = System.currentTimeMillis();
+            
+            log.error(formatter.logError(requestId,target,methodName,startTime,endTime));
+            
+            throw e;
+        }
+    }
+    
+    @Around("execution(* com.beanny.demo.controller..*(..))")
+    public Object logControllerMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+        Logger log = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        String methodName = joinPoint.getSignature().getName();
+        String target = joinPoint.getTarget().getClass().getSimpleName();
+        long startTime = System.currentTimeMillis();
+        String requestId = UUID.randomUUID().toString();
+        
+        log.info(formatter.logRequest(requestId,target,methodName,startTime));
+        
+        try {
+            // execute the original method logic
+            Object result = joinPoint.proceed();
+            
+            long endTime = System.currentTimeMillis();
+            // logging
+            log.info(formatter.logResponse(requestId,target,methodName,startTime,endTime));
+            
+            return result;
+        } catch (Exception e) {
+            long endTime = System.currentTimeMillis();
+            
+            log.error(formatter.logError(requestId,target,methodName,startTime,endTime));
             
             throw e;
         }
