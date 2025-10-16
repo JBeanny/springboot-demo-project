626b6d03abda7af638119d8ef882535e957ef894
Yim Sotharoth <yimsotharoth999@gmail.com>
2025-10-16 22:06:47 +0700

    test(webclient): integrate to JSONPlaceholder API

---
+++ src/main/java/com/beanny/demo/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.beanny.demo.controller;
+
+import com.beanny.demo.dto.base.Response;
+import com.beanny.demo.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/mock")
+public class TestController {
+    @Autowired
+    private TestService testService;
+    
+    @GetMapping
+    public ResponseEntity<Object> testSyncApi() {
+        Object res = testService.testSyncApi();
+        
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(Response.success("200","success","success",res));
+    }
+}
